15ced95ebb43f182c9db24ac94706388a050fae5
SamyC-Dev <cosamy2@gmail.com>
2020-11-18 12:00:37 +0100

    WIP: Add text content in about component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,7 +17,7 @@ function About() {
                                 <div className="field">
                                     <label htmlFor="description" className="label">Description</label>
                                     <p name="description">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ipsam repudiandae dolor ab, neque voluptatem a expedita nam possimus quas nostrum esse, cupiditate aspernatur iusto doloremque facilis
+                                        Je suis Samy Courouble, en reconversion professionnelle, j'ai quitté les chantiers ferroviaires où j'occupais le poste de chef d'équipe pour devenir développeur web et mobile. Ce petit projet a été créé pour alimenter mon portfolio. N'hésitez pas à me contacter pour d'éventuels renseignements.
                                     </p>
                                 </div>
                                 <hr className="has-background-primary my-4" style={{ width: '20%', margin: '0 auto' }} />
