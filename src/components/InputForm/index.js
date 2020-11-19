4abc2293620f131d010b03c2ff8c20093f800dc8
SamyC-Dev <cosamy2@gmail.com>
2020-11-19 16:49:27 +0100

    WIP: Add submit with touch enter

--- src/components/InputForm/index.js
+++ src/components/InputForm/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const InputForm = ({ inputValue, setInputValue, placeholder, handleSubmit, textSubmitButton, colorButton }) => {
 
     return (
-        <div className="field has-addons">
+        <form className="field has-addons">
             <div className="control is-expanded">
                 <input
                     value={inputValue}
@@ -13,9 +13,9 @@ const InputForm = ({ inputValue, setInputValue, placeholder, handleSubmit, textS
                     placeholder={placeholder} />
             </div>
             <div className="control">
-                <button onClick={handleSubmit} className={`button is-${colorButton}`} disabled={inputValue.length < 1 ? true : false} >{textSubmitButton}</button>
+                <button onSubmit={handleSubmit} onClick={handleSubmit} className={`button is-${colorButton}`} disabled={inputValue.length < 1 ? true : false} >{textSubmitButton}</button>
             </div>
-        </div>
+        </form>
     );
 };
 
